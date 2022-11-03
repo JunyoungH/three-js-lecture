75a2548708c257feaa5306acf3c38f9bc76bdd91
Junyoung Hwang <kensiro5218@gmail.com>
2022-11-04 00:02:08 +0900

    update lecture01

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # three-js-lecture
-Repo for three.js personal learning
-
-[three.js로 시작하는 3D 인터랙티브 웹](https://www.inflearn.com/course/3d-%EC%9D%B8%ED%84%B0%EB%9E%99%ED%8B%B0%EB%B8%8C-%EC%9B%B9/)
+Repo for threeJS personal learning
---
+++ webpack.common.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: '/src/index.ts',
+    output: {
+        path: path.join(__dirname, './dist/src'),
+        filename: '[name].js'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './dist/src'),
+        },
+        hot: true,
+    },
+    resolve: {
+        modules:['node_modules'],
+        extensions:['.ts','json','.scss','.css','.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            showErrors: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
+    
+}
